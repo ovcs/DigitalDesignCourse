c488a8dd3f0906d28fff47f9c1017135385e0d9f
OvcS <gerkules007@yahoo.com>
2022-10-12 12:28:08 +0300

    del comments

--- HWReflection/HWSaveFile/Program.cs
+++ HWReflection/HWSaveFile/Program.cs
@@ -7,14 +7,8 @@ string outputPath = Directory.GetCurrentDirectory() + @"\test.txt";
 string dllAssembly = Directory.GetCurrentDirectory() + @"\LoadFileAndCount.dll";
 Serialize ser = new Serialize();
 
-/*var type = typeof(Serialize).Assembly.GetType("СountWords");*/
-/*Assembly dllAssembly = Assembly.LoadFrom("LoadFileAndCount");*/
 Assembly a = Assembly.LoadFrom(dllAssembly);
-/*var types = typeof(Serialize).Assembly.GetTypes();*/
 var type = a.GetTypes().First(e => e.Name == "СountWords");
-/*var meth = type.GetMethod("GetResult", BindingFlags.NonPublic | BindingFlags.Instance);
-var dict = meth.ReturnType;*/
-/*var act = Activator.CreateInstance(dict);*/
 
 var service = Activator.CreateInstance(type);
 var predict = type.InvokeMember("CalcWordsFromFile", BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance, Type.DefaultBinder, service, new Object[] { inputPath });
